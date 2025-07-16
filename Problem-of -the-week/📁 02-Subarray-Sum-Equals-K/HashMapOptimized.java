8cbbd3262b788b7faf0707b92b66d47233f2e2cb
Anshika Bhadauria <anshikabhadauria1807@gmail.com>
2025-07-16 23:05:59 +0530

    Update HashMapOptimized.java

--- Problem-of -the-week/📁 02-Subarray-Sum-Equals-K/HashMapOptimized.java
+++ Problem-of -the-week/📁 02-Subarray-Sum-Equals-K/HashMapOptimized.java
@@ -1,29 +1,19 @@
 import java.util.HashMap;
 
-public class HashMapOptimized {
-    public static int subarraySum(int[] nums, int k) {
-        HashMap<Integer, Integer> prefixSumFreq = new HashMap<>();
-        prefixSumFreq.put(0, 1); 
-
-        int count = 0;
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> prefixCount = new HashMap<>();
         int prefixSum = 0;
-
-        for (int num : nums) {
-            prefixSum += num;
-
-            if (prefixSumFreq.containsKey(prefixSum - k)) {
-                count += prefixSumFreq.get(prefixSum - k);
+        int count = 0;
+        prefixCount.put(0, 1);   
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum += nums[i];
+            if (prefixCount.containsKey(prefixSum - k)) {
+                count += prefixCount.get(prefixSum - k);
             }
-
-            prefixSumFreq.put(prefixSum, prefixSumFreq.getOrDefault(prefixSum, 0) + 1);
+            prefixCount.put(prefixSum, prefixCount.getOrDefault(prefixSum, 0) + 1);
         }
-
+        
         return count;
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3};
-        int k = 3;
-        System.out.println(subarraySum(nums, k)); // Output: 2
     }
 }
